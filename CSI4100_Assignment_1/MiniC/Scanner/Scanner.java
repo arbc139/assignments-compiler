17dd5e73545d6815eaaddbd5b416a1b04bac1844
totoro <arbc139@gmail.com>
2017-10-13 20:31:46 +0900

    Implements separaters.

--- CSI4100_Assignment_1/MiniC/Scanner/Scanner.java
+++ CSI4100_Assignment_1/MiniC/Scanner/Scanner.java
@@ -38,6 +38,14 @@ public final class Scanner {
       "[\\=\\|\\&\\!\\<\\>\\+\\-\\*\\/]", String.valueOf(c));
   }
 
+  private boolean isSeperator(char c) {
+    return Pattern.matches("[\\{\\}\\(\\)\\[\\]\\,\\;]", String.valueOf(c));
+  }
+
+  private boolean isFractionBreakPoint(char c) {
+    return Pattern.matches("[\\.eE]", String.valueOf(c));
+  }
+
 ///////////////////////////////////////////////////////////////////////////////
 
   public Scanner(SourceFile source) {
@@ -66,85 +74,138 @@ public final class Scanner {
   }
 
   private int scanOperator() {
+    takeIt();
     switch (currentChar) {
       case '=':
-        takeIt();
         if (currentChar == '=') {
           takeIt();
           return Token.EQ;
         }
         return Token.ASSIGN;
       case '|':
-        takeIt();
         if (currentChar == '|') {
           takeIt();
           return Token.OR;
         }
         return Token.ERROR;
       case '&':
-        takeIt();
         if (currentChar == '&') {
           takeIt();
           return Token.AND;
         }
         return Token.ERROR;
       case '!':
-        takeIt();
         if (currentChar == '=') {
           takeIt();
           return Token.NOTEQ;
         }
         return Token.NOT;
       case '<':
-        takeIt();
         if (currentChar == '=') {
           takeIt();
           return Token.LESSEQ;
         }
         return Token.LESS;
       case '>':
-        takeIt();
         if (currentChar == '=') {
           takeIt();
           return Token.GREATEREQ;
         }
         return Token.GREATER;
       case '+':
-        takeIt();
         return Token.PLUS;
       case '-':
-        takeIt();
         return Token.MINUS;
       case '*':
-        takeIt();
         return Token.TIMES;
       case '/':
-        takeIt();
         return Token.DIV;
       default:
-        takeIt();
         return Token.ERROR;
     }
   }
 
+  private int scanSepearator() {
+    takeIt();
+    switch (currentChar) {
+      case '{':
+        return Token.LEFTBRACE;
+      case '}':
+        return Token.RIGHTBRACE;
+      case '(':
+        return Token.LEFTBRACKET;
+      case ')':
+        return Token.RIGHTBRACKET;
+      case '[':
+        return Token.LEFTPAREN;
+      case ']':
+        return Token.RIGHTPAREN;
+      case ';':
+        return Token.SEMICOLON;
+      default:
+        return Token.ERROR;
+    }
+  }
+
+  private int getKeywordToken(String currentLexemeStr) {
+    switch (currentLexemeStr) {
+      case "bool":
+        return Token.BOOL;
+      case "else":
+        return Token.ELSE;
+      case "for":
+        return Token.FOR;
+      case "float":
+        return Token.FLOAT;
+      case "if":
+        return Token.IF;
+      case "int":
+        return Token.INT;
+      case "return":
+        return Token.RETURN;
+      case "void":
+        return Token.VOID;
+      case "while":
+        return Token.WHILE;
+      case "true":
+        // Fall through...
+      case "false":
+        return Token.BOOLLITERAL;
+      default:
+        return Token.ID;
+    }
+  }
+
+  private int scanIdentifierKeywordBoolLiteral() {
+    takeIt();
+    while (isLetter(currentChar) || isDigit(currentChar)) {
+      takeIt();
+    }
+    return getKeywordToken(currentLexeme.toString());
+  }
+
+  private int scanIntFloatLiteral() {
+    takeIt();
+    while (isDigit(currentChar)) {
+      takeIt();
+    }
+    if (!isFractionBreakPoint(currentChar)) {
+      return Token.INTLITERAL;
+    }
+    // Fraction handling...
+    // TODO(totoro): Fraction 뒤에 핸들링 구현해야햄 알아찌 내 자신아? (하트)
+  }
+
   private int scanToken() {
     int tokenType = Token.ERROR;
-
     if (isOperator(currentChar)) {
       tokenType = scanOperator();
+    } else if (isSeperator(currentChar)) {
+      tokenType = scanSepearator();
     } else if (isLetter(currentChar)) {
-      takeIt();
-      while (isLetter(currentChar) || isDigit(currentChar)) {
-        takeIt();
-      }
-      tokenType = Token.ID;
+      tokenType = scanIdentifierKeywordBoolLiteral();
     } else if (isDigit(currentChar)) {
-      takeIt();
-      while (isDigit(currentChar)) {
-        takeIt();
-      }
-      // Note: code for floating point literals is missing here...
-      tokenType = Token.INTLITERAL;
+      tokenType = scanIntFloatLiteral();
     } else if (currentChar == '\u0000') {
       currentLexeme.append('$');
       tokenType = Token.EOF;

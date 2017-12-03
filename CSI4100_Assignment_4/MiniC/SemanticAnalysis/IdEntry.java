f420e67f31860eacfacce77c3730c41e89874a69
arbc139 <arbc139@gmail.com>
2017-12-03 15:17:02 +0900

    Fixes code style issues.

--- CSI4100_Assignment_4/MiniC/SemanticAnalysis/IdEntry.java
+++ CSI4100_Assignment_4/MiniC/SemanticAnalysis/IdEntry.java
@@ -4,16 +4,15 @@ import MiniC.AstGen.Decl;
 
 public class IdEntry {
 
-    protected String id;
-    protected Decl declAST;
-    protected int level;
-    protected IdEntry previous;
+  protected String id;
+  protected Decl declAST;
+  protected int level;
+  protected IdEntry previous;
 
-    public IdEntry (String id, Decl declAST, int level, IdEntry previous) {
-	this.id = id;
-	this.declAST = declAST;
-	this.level = level;
-	this.previous = previous;
-    }
-
+  public IdEntry (String id, Decl declAST, int level, IdEntry previous) {
+    this.id = id;
+    this.declAST = declAST;
+    this.level = level;
+    this.previous = previous;
+  }
 }
